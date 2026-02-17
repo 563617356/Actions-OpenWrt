7788b13226e3a607d6db4a5a43b10061a6a6fe3b
563617356 <74829951+563617356@users.noreply.github.com>
2026-02-17 12:39:40 -0500

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,21 +4,28 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认 IP (根据需要修改，这里改为 192.168.1.1)
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 修改主机名为 JDCloud
+sed -i 's/OpenWrt/JDCloud-One/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 【核心修复】修正 5G 信号 EEPROM 偏移量
+# 解决闭源驱动下 5G 信号只有 6dBm 的问题
+DTS_FILE="target/linux/ramips/dts/mt7621_jdcloud_re-sp-01b.dts"
+if [ -f "$DTS_FILE" ]; then
+    # 先删除可能存在的错误定义，防止重复
+    sed -i '/mediatek,mtd-eeprom = <&factory 0x0>/d' $DTS_FILE
+    sed -i '/mediatek,mtd-eeprom = <&factory 0x8000>/d' $DTS_FILE
+    # 在 mt7615 节点下精准插入正确的偏移量
+    sed -i '/mt7615@0,0 {/a \\t\t\tmediatek,mtd-eeprom = <&factory 0x8000>;' $DTS_FILE
+    echo "DTS EEPROM Offset Fixed!"
+fi
 
-# 修改京东云一代 5G 信号 EEPROM 偏移量 (从默认 0x0 修正为 0x8000)
-# 这一步是解决 6dBm 的关键
-sed -i 's/mediatek,mtd-eeprom = <&factory 0x0>/mediatek,mtd-eeprom = <&factory 0x8000>/g' target/linux/ramips/dts/mt7621_jdcloud_re-sp-01b.dts
+# 4. 解锁无线功率限制 (设置为美国区域 US)
+# 这一步能让闭源驱动在初始化时跳过国内较严的功率限制
+sed -i 's/option country .*/option country '\''US'\''/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh 2>/dev/null || true
+
+# 5. 修复部分闭源驱动界面显示中文乱码 (可选)
+# sed -i 's/default \"UTF-8\"/default \"zh-cn\"/g' feeds/luci/modules/luci-base/luasrc/i18n.lua
